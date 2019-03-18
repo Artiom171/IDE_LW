3dee96826e5465ade8e6941191e28acf3596374f
Artiom171 <artiom201097@gmail.com>
2019-03-18 23:04:20 +0200

    Student object, List for homewokr marks, sorted list

--- Student.cs
+++ Student.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 
+
+
 namespace C__LD
 {
 
@@ -23,25 +25,41 @@ namespace C__LD
 			student.Surname = Console.ReadLine();
 			
 			Console.WriteLine();
+//			Naudojant paprasta masyva
+//			-------------------------
 //			Console.WriteLine("Dabar iveskite atliktu namu darbu skaiciu");
 //			int homeWorkCount = Convert.ToInt32(Console.ReadLine());
 			
-			double[] marks = new double[100];
-			double homeWorkSum = 0;
+//			double[] marks = new double[100];
+			int homeWorkSum = 0;
 			double homeWorkAvg = 0;
+			var marks = new List<int>();
 			
 			Console.WriteLine();
-			Console.WriteLine("Dabar iveskite atliktu namu darbu pazymius,\n jeigu daugiau pazymiu nera iveskite 0");
-			int count = 0;
-			for (int i = 0; i < 100; i++) {
-				marks[i] = Convert.ToDouble(Console.ReadLine());
-				homeWorkSum += marks[i];
-				if(Convert.ToDouble(Console.ReadLine()).Equals(0)){
-					count = i+1;
-					break;
-				}
+			Console.WriteLine("Dabar iveskite atliktu namu darbu pazymius,\njeigu daugiau pazymiu nera iveskite 0");
+			int notZero = 0;
+			do {
+				notZero = Convert.ToInt32(Console.ReadLine());
+				marks.Add(notZero);
+			} while (notZero != 0);
+			marks.Sort();
+
+			foreach (var item in marks) {
+				homeWorkSum += item;
 			}
-			homeWorkAvg = homeWorkSum / count;
+			
+//          Naudojant paprasta masyva
+//			-------------------------
+//			int count = 0;
+//			for (int i = 0; i < 100; i++) {
+//				marks[i] = Convert.ToDouble(Console.ReadLine());
+//				homeWorkSum += marks[i];
+//				if(Convert.ToDouble(Console.ReadLine()).Equals(0)){
+//					count = i+1;
+//					break;
+//				}
+//			}
+			homeWorkAvg = homeWorkSum / marks.Count;
 			Console.WriteLine();
 			
 			Console.WriteLine("Iveskite egzamino rezultata:");
